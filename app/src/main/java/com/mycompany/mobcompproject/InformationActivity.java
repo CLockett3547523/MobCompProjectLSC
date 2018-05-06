ff4ad00779fa20a5fc6fc0373afeb40521529f02
Cameron Lockett <s3547523@student.rmit.edu.au>
2018-05-06 18:13:33 +1000

    Bluetooth is not finished. Need to implement server and client as well as find UUID method

--- app/src/main/java/com/mycompany/mobcompproject/InformationActivity.java
+++ app/src/main/java/com/mycompany/mobcompproject/InformationActivity.java
@@ -46,4 +46,11 @@ public class InformationActivity extends AppCompatActivity {
         startActivityForResult(myIntent, REQUEST_CODE);
         // come back via onResume() event.
     }
+
+    /*  Enable discovery via Bluetooth
+    Intent discoverableIntent =
+        new Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE);
+discoverableIntent.putExtra(BluetoothAdapter.EXTRA_DISCOVERABLE_DURATION, 300);
+startActivity(discoverableIntent);
+     */
 }
---
+++ app/src/main/java/com/mycompany/mobcompproject/MainActivity.java
@@ -0,0 +1,30 @@
+package com.mycompany.mobcompproject;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void CheckInClick(View view) {// function named in OnClick
+        startActivity(new Intent(MainActivity.this, InformationActivity.class));
+        // come back via onResume() event.
+    }
+
+    public void SensorClick(View view) {// function named in OnClick
+        startActivity(new Intent(MainActivity.this, SensorActivity.class));
+        // come back via onResume() event.
+    }
+
+    public void SnapshotClick(View view) {// function named in OnClick
+        startActivity(new Intent(MainActivity.this, SnapshotActivity.class));
+        // come back via onResume() event.
+    }
+}
